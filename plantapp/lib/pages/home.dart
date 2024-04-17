725243e1c428ee574786a9afd67d962ff05216a0
amri-tah <amrithanandini2003@gmail.com>
2024-04-17 15:43:21 +0530

    home page design done

--- plantapp/lib/pages/home.dart
+++ plantapp/lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:plantapp/pages/homebuttons.dart';
 import 'package:plantapp/pages/weatherdets.dart';
 
 class Home extends StatelessWidget {
@@ -12,6 +13,7 @@ class Home extends StatelessWidget {
     return Scaffold(
         extendBodyBehindAppBar: true,
       appBar: AppBar(
+
         title:Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
@@ -19,90 +21,92 @@ class Home extends StatelessWidget {
               Text("PlantPulse", style: GoogleFonts.poppins(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 25),),
             ],
           ),
-        ), backgroundColor: Colors.transparent, actions: [Padding(
+        ), backgroundColor: Color.fromRGBO(161, 207, 107, 1), actions: [Padding(
         padding: const EdgeInsets.all(15.0),
         child: Icon(Icons.person, color: Colors.white,),
       )],),
-      body: Expanded(
-        child: Column(
-          children: [Stack(
-            alignment: Alignment.bottomLeft,
-            children: [Container(
-                height: 250,
-                width: 500,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(81)),
-                  gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color.fromRGBO(161, 207, 107, 1), Color.fromRGBO(74, 173, 82, 1)])
-                ),
+      body: ListView(
+        children: [Stack(
+          alignment: Alignment.bottomLeft,
+          children: [Container(
+              height: 150,
+              width: 500,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(81)),
+                gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Color.fromRGBO(161, 207, 107, 1), Color.fromRGBO(74, 173, 82, 1)])
               ),
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Wrap(
-                  direction: Axis.vertical,
-                  children: [Text.rich(TextSpan(
-                    text: 'Welcome \n', // First part of the text
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      height:0.9,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 28,
-                    ),
-                    children: <TextSpan>[
-                    TextSpan(
-                      text: 'Username!', // Second part of the text
-                      style: GoogleFonts.poppins(
-                        height:0.8,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600, // Different font weight
-                        fontSize: 35, // Same font size, or adjust as needed
-                      ),
-                    ),
-        
-        
-                    ],
-                  )),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Flex(
-                        direction: Axis.horizontal,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                          Icon(Icons.location_pin, color: Colors.white,),
-                        Text("Coimbatore, India", style: GoogleFonts.poppins(color: Colors.white),)
-                      ],
-                                    ),
-                    )]
-                ),
-        
-        
-              )
-        
-            ]
-          ),
-
+            ),
             Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white, boxShadow: [new BoxShadow(
-                  color: Colors.grey.shade300,
-                  blurRadius: 35,
-                ),]
-                ),
-                
-                child: GridView.count(crossAxisCount: 2, childAspectRatio: 2, shrinkWrap: true, padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                children: [WeatherDetails(wtype: "Temperature", val: "30°C", ic: Icons.thermostat),
-                        WeatherDetails(wtype: "Humidity", val: "36%", ic: Icons.water),
-                        WeatherDetails(wtype: "Rainfall", val: "0mm", ic: Icons.water_drop),
-                        WeatherDetails(wtype: "Wind Speed", val: "11 mph", ic: Icons.wind_power_rounded)],),
+              padding: const EdgeInsets.all(25.0),
+              child: Wrap(
+                direction: Axis.vertical,
+                children: [Text.rich(TextSpan(
+                  text: 'Welcome \n', // First part of the text
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    height:0.9,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 28,
+                  ),
+                  children: <TextSpan>[
+                  TextSpan(
+                    text: 'Username!', // Second part of the text
+                    style: GoogleFonts.poppins(
+                      height:0.8,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600, // Different font weight
+                      fontSize: 35, // Same font size, or adjust as needed
+                    ),
+                  ),
+
+
+                  ],
+                )),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Flex(
+                      direction: Axis.horizontal,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        Icon(Icons.location_pin, color: Colors.white,),
+                      Text("Coimbatore, India", style: GoogleFonts.poppins(color: Colors.white),)
+                    ],
+                                  ),
+                  )]
               ),
+
+
             )
 
-
-
-          ],
+          ]
         ),
+
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white, boxShadow: [new BoxShadow(
+                color: Colors.grey.shade300,
+                blurRadius: 35,
+              ),]
+              ),
+
+              child: GridView.count(crossAxisCount: 2, childAspectRatio: 2, shrinkWrap: true, padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10, ), physics: NeverScrollableScrollPhysics(),
+              children: [WeatherDetails(wtype: "Temperature", val: "30°C", ic: Icons.thermostat),
+                      WeatherDetails(wtype: "Humidity", val: "36%", ic: Icons.water),
+                      WeatherDetails(wtype: "Rainfall", val: "0mm", ic: Icons.water_drop),
+                      WeatherDetails(wtype: "Wind Speed", val: "11 mph", ic: Icons.wind_power_rounded)],),
+            ),
+          ),
+          ButtonsHome(imgpath: "lib/images/ndvi.png", heading: "Satellite View",),
+
+          ButtonsHome(imgpath: "lib/images/iot.jpg", heading: "In Ground Sensors",),
+
+
+
+
+        ],
       ),
 
 
---
+++ plantapp/lib/pages/homebuttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ButtonsHome extends StatelessWidget {
+  final String heading;
+  final String imgpath;
+  const ButtonsHome({super.key, required this.heading, required this.imgpath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+      child: Container(
+        width: 350,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white, boxShadow: [new BoxShadow(
+          color: Colors.grey.shade300,
+          blurRadius: 30,
+        ),]
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                    width:350, height:180,clipBehavior: Clip.antiAlias,
+                    child: Image.asset(imgpath, fit: BoxFit.cover, )),
+              ),
+              Text(heading, style: GoogleFonts.poppins())
+
+            ],
+
+
+          ),
+        ),
+      ),
+    );
+  }
+}
