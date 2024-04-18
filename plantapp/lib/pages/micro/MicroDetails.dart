99ac15668ea6969a445a411d4726b06bafa21c9c
amri-tah <amrithanandini2003@gmail.com>
2024-04-18 11:44:58 +0530

    sensors page design done

--- plantapp/lib/pages/micro/MicroDetails.dart
+++ plantapp/lib/pages/micro/MicroDetails.dart
@@ -7,6 +7,14 @@ class MicroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add, color: Colors.white,size: 32,),
+        backgroundColor: Color.fromRGBO(0, 100, 53, 1),
+
+        shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(100),
+      )),
       appBar: AppBar(
         iconTheme: IconThemeData(
           color: Colors.white, // This sets the color of the leading icon to white
@@ -44,7 +52,67 @@ class MicroPage extends StatelessWidget {
             )),
           )
 
-        ])]),
+        ]),
+
+
+      SizedBox(height: 10),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+        child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 100, 53, 1),
+            borderRadius: BorderRadius.circular(20)
+          ),
+
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Wrap(
+                  direction: Axis.vertical,
+                  children: [
+                    Text("Node 1", style: GoogleFonts.poppins(
+                      height:1,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600, // Different font weight
+                      fontSize: 25, // Same font size, or adjust as needed
+                    ),),
+                    Text("Wheat", style: GoogleFonts.poppins(
+                      height:1,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal, // Different font weight
+                      fontSize: 15, // Same font size, or adjust as needed
+                    ),)
+                  ],
+                ),
+
+                Container(
+                  height: 80,
+                  width: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(80)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset("lib/images/wheat.png"),
+                  ),
+
+                )
+              ],
+            ),
+          ),
+          
+        ),
+      )
+
+
+    ]
+
+      ),
     );
   }
 }
