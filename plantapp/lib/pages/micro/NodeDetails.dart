52b6fd2fe3f6335e8adc762898156938b0e32475
amri-tah <amrithanandini2003@gmail.com>
2024-05-22 16:14:03 +0530

    ml model working

--- plantapp/lib/pages/micro/NodeDetails.dart
+++ plantapp/lib/pages/micro/NodeDetails.dart
@@ -2,9 +2,59 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:plantapp/pages/micro/sensordets.dart';
+import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
 
-class NodeDetails extends StatelessWidget {
+class NodeDetails extends StatefulWidget {
   const NodeDetails({super.key});
+
+  @override
+  State<NodeDetails> createState() => _NodeDetailsState();
+}
+
+class _NodeDetailsState extends State<NodeDetails> {
+  List result = [];
+  int sensedtemp = 35;
+  int sensedhumidity = 60;
+  double sensedrainfall = 100;
+
+  String predicted = "";
+  final List<String> labels = ['rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas',
+    'mothbeans', 'mungbean', 'blackgram', 'lentil', 'pomegranate', 'banana', 'mango',
+    'grapes', 'watermelon', 'muskmelon', 'apple', 'orange', 'papaya',
+    'coconut', 'cotton', 'jute', 'coffee'];
+
+  runModel(temp, humd, rain) async {
+    final interpreter = await tfl.Interpreter.fromAsset('lib/mlmodel/linearRegrssionModel.tflite');
+    //prepare input as required by your model
+    //in my case it required a single number and i am requiring it from user
+    //passing user entered number to model as input
+    final input = [
+      [temp, humd, rain]
+    ];
+    //Prepare output set and use reshape method from the plugin.
+    //Input and output preparing guide comes with the model documentation.
+    var output = List.filled(22, 0).reshape([1, 22]);
+    //run the interpreter on prepared input and output.
+    interpreter.run(input, output);
+    //model process the input and updates the output
+    //Result variable to show output to user
+    result = output[0];
+    print(result);
+
+    int maxIndex = 0;
+    double maxValue = result[0];
+    for (int i = 1; i < result.length; i++) {
+      if (result[i] > maxValue) {
+        maxIndex = i;
+        maxValue = result[i];
+      }
+    }
+
+    setState(() {
+      predicted = labels[maxIndex];
+      print(predicted);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +104,7 @@ class NodeDetails extends StatelessWidget {
             child: Container(
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
+                  gradient: const LinearGradient(
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
                       colors: [
@@ -62,27 +112,37 @@ class NodeDetails extends StatelessWidget {
                         Color.fromRGBO(74, 173, 82, 1)
                       ])),
               child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 40.0),
+                padding: EdgeInsets.symmetric(vertical: 40.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     SensorDetails(
-                      sensedval: "25°C",
+                      sensedval: "$sensedtemp°C",
                       icon: Icons.thermostat,
                       stype: "Temperature",
                     ),
                     SensorDetails(
-                      sensedval: "60%",
+                      sensedval: "$sensedhumidity%",
                       icon: Icons.water_drop,
-                      stype: "Soil Moisture",
-                    )
+                      stype: "Humidity",
+                    ),
+
                   ],
                 ),
               ),
             ),
           ),
           ElevatedButton(
-              onPressed: () {},
+            // TO DO: Add model feature here
+              onPressed: () {
+                runModel(sensedtemp, sensedhumidity, sensedrainfall);
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                backgroundColor: Color.fromRGBO(74, 173, 82, 1),
+              ),
               child: Text(
                 "Get Crop Recommendation",
                 style: GoogleFonts.poppins(
@@ -91,12 +151,6 @@ class NodeDetails extends StatelessWidget {
                   fontWeight: FontWeight.normal, // Different font weight
                   fontSize: 17, // Same font size, or adjust as needed
                 ),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                backgroundColor: Color.fromRGBO(74, 173, 82, 1),
               )),
           SizedBox(height: 30),
           Column(
